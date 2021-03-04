1c47a6c006ab6df41964917b1e52375b2e0667b6
Gokul Swami <gokulswamilive@gmail.com>
2021-03-03 17:33:22 -0800

    removed negative

--- commands/bot-stuff/ping.js
+++ commands/bot-stuff/ping.js
@@ -6,7 +6,7 @@ module.exports = {
         const emoji = message.client.emojis.cache.get('816360102772342825')
 
         const apiPing = Math.round(message.client.ws.ping);
-        const responseTime = -1 * Math.round(Date.now() - message.createdTimestamp);
+        const responseTime = Math.round(Date.now() - message.createdTimestamp);
 
         message.channel.send(`Pong! Latency is ${responseTime}ms, and the API latency is ${apiPing}ms ${emoji}`);
     },
