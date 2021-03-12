dc9fab3cec03b812fd36c4e1dbca4676e9d8d025
Jimmy <69643443+Jimmysit0@users.noreply.github.com>
2021-03-12 07:57:33 -0500

    Update say.js

--- commands/bot/say.js
+++ commands/bot/say.js
@@ -1,12 +1,15 @@
-const { prefix } = require('../../config.json');
-
 module.exports = {
     name: 'say',
     aliases: ['msg', 'talk'],
     description: 'Talk as the bot',
     execute(message, args) {
-        var ch = message.channel;
-        message.delete();
-        ch.send(message.content.substring(prefix.length + 3)); 
+         if(!args[0]) return message.channel.send({embed: {color: 'RED', description: ':no_entry_sign: | Please tell me what you want me to say!'}});
+        
+         const sayMessage = args.join(" ");
+    
+
+    message.delete().catch(O_o=>{}); 
+
+    message.channel.send(sayMessage);
     },
 };
